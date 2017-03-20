c86d1fdd780a1f7c0e83bfdf93f67fb67641d8ab
Rick <ricksbrown73@gmail.com>
2017-03-20 17:47:46 +1100

    Added basic swagger support.

--- corpdir-srv-web-bundle/src/main/java/com/github/bordertech/corpdir/jersey/config/ApplicationConfig.java
+++ corpdir-srv-web-bundle/src/main/java/com/github/bordertech/corpdir/jersey/config/ApplicationConfig.java
@@ -1,10 +1,12 @@
 package com.github.bordertech.corpdir.jersey.config;
 
+import com.github.bordertech.corpdir.servlet.CorsResponseFilter;
 import org.glassfish.jersey.server.ResourceConfig;
 
 public class ApplicationConfig extends ResourceConfig {
 
 	public ApplicationConfig() {
+		register(CorsResponseFilter.class);
 		register(new ApplicationBinder());
 
 		packages(true, "com.github.bordertech.corpdir.resource");
