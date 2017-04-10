4e4e57b4c30e01f756e2578c9a08966a90b3f59a
Jonathan Austin <jonathan.austin@border.gov.au>
2017-04-10 16:44:11 +1000

    UI Skeleton

---
+++ corpdir-srv-web-api/src/main/java/com/github/bordertech/corpdir/web/api/resource/v1/ContactResource.java
@@ -0,0 +1,131 @@
+package com.github.bordertech.corpdir.web.api.resource.v1;
+
+import com.github.bordertech.corpdir.api.response.BasicResponse;
+import com.github.bordertech.corpdir.api.response.DataResponse;
+import com.github.bordertech.corpdir.api.v1.ContactService;
+import com.github.bordertech.corpdir.api.v1.model.Contact;
+import com.github.bordertech.corpdir.api.v1.model.Position;
+import java.util.List;
+import javax.inject.Inject;
+import javax.ws.rs.DELETE;
+import javax.ws.rs.GET;
+import javax.ws.rs.POST;
+import javax.ws.rs.PUT;
+import javax.ws.rs.Path;
+import javax.ws.rs.PathParam;
+import javax.ws.rs.Produces;
+import javax.ws.rs.QueryParam;
+import javax.ws.rs.core.MediaType;
+
+/**
+ * Contact REST Resource.
+ *
+ * @author Jonathan Austin
+ * @since 1.0.0
+ */
+@Path(value = "v1/contacts")
+public class ContactResource implements ContactService {
+
+	private final ContactService impl;
+
+	/**
+	 * @param impl the service implementation
+	 */
+	@Inject
+	public ContactResource(final ContactService impl) {
+		this.impl = impl;
+	}
+
+	@GET
+	@Produces(MediaType.APPLICATION_JSON)
+	@Override
+	public DataResponse<List<Contact>> search(@QueryParam("search") final String search) {
+		return impl.search(search);
+	}
+
+	@GET
+	@Path("/{key}")
+	@Produces(MediaType.APPLICATION_JSON)
+	@Override
+	public DataResponse<Contact> retrieve(@PathParam("key") final String keyId) {
+		return impl.retrieve(keyId);
+	}
+
+	@POST
+	@Produces(MediaType.APPLICATION_JSON)
+	@Override
+	public DataResponse<Contact> create(final Contact contact) {
+		return impl.create(contact);
+	}
+
+	@PUT
+	@Path("/{key}")
+	@Produces(MediaType.APPLICATION_JSON)
+	@Override
+	public DataResponse<Contact> update(@PathParam("key") final String keyId, final Contact contact) {
+		return impl.update(keyId, contact);
+	}
+
+	@DELETE
+	@Path("/{key}")
+	@Produces(MediaType.APPLICATION_JSON)
+	@Override
+	public BasicResponse delete(@PathParam("key") final String keyId) {
+		return impl.delete(keyId);
+	}
+
+	@GET
+	@Path("/{key}/positions")
+	@Produces(MediaType.APPLICATION_JSON)
+	@Override
+	public DataResponse<List<Position>> getPositions(@PathParam("key") final String keyId) {
+		return impl.getPositions(keyId);
+	}
+
+	@PUT
+	@Path("/{key}/positions/{subKey}")
+	@Produces(MediaType.APPLICATION_JSON)
+	@Override
+	public DataResponse<Contact> addPosition(@PathParam("key") final String keyId, @PathParam("subKey") final String positionKeyId) {
+		return impl.addPosition(keyId, positionKeyId);
+	}
+
+	@DELETE
+	@Path("/{key}/positions/{subKey}")
+	@Produces(MediaType.APPLICATION_JSON)
+	@Override
+	public DataResponse<Contact> removePosition(@PathParam("key") final String keyId, @PathParam("subKey") final String positionKeyId) {
+		return impl.removePosition(keyId, positionKeyId);
+	}
+
+	@Override
+	public DataResponse<byte[]> getImage(final String keyId) {
+		return impl.getImage(keyId);
+	}
+
+	@Override
+	public BasicResponse deleteImage(final String keyId) {
+		return impl.deleteImage(keyId);
+	}
+
+	@Override
+	public BasicResponse setImage(final String keyId, final byte[] image) {
+		return impl.setImage(keyId, image);
+	}
+
+	@Override
+	public DataResponse<byte[]> getThumbnail(final String keyId) {
+		return impl.getThumbnail(keyId);
+	}
+
+	@Override
+	public BasicResponse deleteThumbnail(final String keyId) {
+		return impl.deleteThumbnail(keyId);
+	}
+
+	@Override
+	public BasicResponse setThumbnail(final String keyId, final byte[] image) {
+		return impl.setThumbnail(keyId, image);
+	}
+
+}
---
+++ corpdir-srv-web-ui/src/main/java/com/github/bordertech/corpdir/web/ui/CorpDirApp.java
@@ -0,0 +1,20 @@
+package com.github.bordertech.corpdir.web.ui;
+
+import com.github.bordertech.wcomponents.WApplication;
+import com.github.bordertech.wcomponents.WText;
+
+/**
+ * Corporate Directory Admin UI.
+ *
+ * @author jonathan
+ */
+public class CorpDirApp extends WApplication {
+
+	/**
+	 * Construct App.
+	 */
+	public CorpDirApp() {
+		add(new WText("Hello"));
+	}
+
+}
