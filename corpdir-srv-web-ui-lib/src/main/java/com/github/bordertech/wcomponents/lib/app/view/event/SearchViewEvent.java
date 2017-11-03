bbcc000e1f584e0aa17061ad3fdd6d0cf25fdd80
Jonathan Austin <jonathan.austin@border.gov.au>
2017-11-03 17:28:37 +1100

    base events

---
+++ corpdir-srv-web-ui-lib/src/main/java/com/github/bordertech/wcomponents/lib/app/view/event/SearchViewEvent.java
@@ -0,0 +1,12 @@
+package com.github.bordertech.wcomponents.lib.app.view.event;
+
+import com.github.bordertech.flux.event.ViewEventType;
+
+/**
+ * Search view events.
+ *
+ * @author Jonathan Austin
+ * @since 1.0.0
+ */
+public interface SearchViewEvent extends ViewEventType {
+}
